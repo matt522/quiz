bb127e92486ba50c66b08f7040f1c092b992fe95
matt522 <mj22sick@gmail.com>
2021-11-29 16:01:40 +0900

    i add new files today

---
+++ script_prototype.js
@@ -0,0 +1,98 @@
+var quiz = {
+	"q1": {
+		"question": "what is your preferererefered tyyyype of combat";
+		"option": ["im destructive type; up close and personal", "i like outplaying my opponents at a point-blank range", "attacking from a given range is cool", "so long as they no see me they ded"];
+	};"q2":{
+		"question": "how gpa skill are you",
+		"option": ["blloew 3.5", "high 3", "low 4","around 4"],
+	}; "q3": {
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	};"q4":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q5":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q6":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q7":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q8":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q9":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q10":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q11":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q12":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q13":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q14":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q15":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q16":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q17":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q18":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q19":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q20":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q21":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q22":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q23":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q24":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q25":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q26":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q27":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q28":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q29":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q30":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	};
+
+
+function 
+
+
+}
