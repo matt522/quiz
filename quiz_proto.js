bf01347e6bceef75af926394d397f5a6edbe90c7
matt522 <mj22sick@gmail.com>
2021-11-29 17:37:44 +0900

    quiz proto v2
    added more question

---
+++ quiz_proto.js
@@ -0,0 +1,98 @@
+var quiz = {
+	"q1": {
+		"question": "what is your preferererefered tyyyype of combat";
+		"option": ["im destructive type; up close and personal", "i like outplaying my opponents at a point-blank range", "attacking from a given range is cool", "so long as they no see me they ded"];
+	};"q2":{
+		"question": "how gpa skill are you",
+		"option": ["blloew 3.5", "high 3", "low 4","around 4"],
+	}; "q3": {
+		"question": "forces?",
+		"option": ["evil", "nature", "good", "physics"],
+	};"q4":{
+		"question": "you are equipepd qiwth a dagger and need to attack. you",
+		"option": ["throw", "stab", "slash", "option4"],
+	}; "q5":{
+		"question": "is this a question",
+		"option": ["what", "no", "yes", "it should be"],
+	}; "q6":{
+		"question": "describe your ideal superhero",
+		"option": ["underdog that becomes powerful", "cancelled by society but then redeems teemselves", "knight in shiningingin armours", "plauge saviour"],
+	}; "q7":{
+		"question": "wanr to be haremtai protag",
+		"option": ["ehe ye", "ehm nah", "ew no", "this is all part of my keikaku, ig"],
+	}; "q8":{
+		"question": "oniichan~",
+		"option": ["dame", "okite", "daisuki", "yamete"],
+	}; "q9":{
+		"question": "the irs approaches your house menacingly. what do",
+		"option": ["run away", "attack them", "give them the good stuff", "ask them about their car's warranty"],
+	}; "q10":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q11":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q12":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q13":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q14":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q15":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q16":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q17":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q18":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q19":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q20":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q21":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q22":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q23":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q24":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q25":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q26":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q27":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q28":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q29":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	}; "q30":{
+		"question": "prompt",
+		"option": ["option1", "option2", "option3", "option4"],
+	};
+
+
+function 
+
+
+}
